034474279e143fa8de7482ebaaecc8f45f9462c8
Ubuntu <rwyat002@ucr.edu>
2020-05-27 13:37:46 -0700

    turnin changes

---
+++ ping.h
@@ -0,0 +1,27 @@
+
+//define local variable(s) here.  Use static keyword to keep local, e.g:
+//   static int i;  // defines a local int named i
+static unsigned char ping_c;
+
+
+/*complete the state machine*/
+
+void Ping()
+{
+
+    switch(ping_state)
+    {
+        case PInit:
+            break;
+        default:
+            ping_state = PInit;
+            break;
+    }
+    switch(ping_state)
+    {
+        case PInit:
+            break;
+        default:
+            break;
+    }
+}
